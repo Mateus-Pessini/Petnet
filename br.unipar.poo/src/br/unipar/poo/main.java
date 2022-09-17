44d705f6fca6288533e80ca05083a875d8ba745f
kauahenrique07 <kaua.siqueira@edu.unipar.br>
2022-09-17 19:43:26 -0300

    Adicionando novos testes

--- br.unipar.poo/src/br/unipar/poo/main.java
+++ br.unipar.poo/src/br/unipar/poo/main.java
@@ -1,4 +1,3 @@
-
 package br.unipar.poo;
 
 import br.unipar.poo.models.Animal;
@@ -18,7 +17,6 @@ import br.unipar.poo.models.Vacina;
 public class main {
 
     public static void main(String[] args) {
-        
 
         Pais pais = new Pais(1, "Brasil");
         Estado estado = new Estado(1, "Paraná", pais);
@@ -27,15 +25,19 @@ public class main {
         Endereco endereco2 = new Endereco("Rua Paranaue", 2, cidade1, 2224);
         Endereco endereco3 = new Endereco("Rua 25 de Dezeembro", 3, cidade1, 2225);
         Endereco endereco4 = new Endereco("Rua Thanos", 4, cidade1, 2226);
-        Proprietario proprietario1 = new Proprietario("585.837.370-45", 1, "Kaua Pessini Sito", "(45)99808-7706", "07/06/2003", endereco1);
-        Proprietario proprietario2 = new Proprietario("583.834.375-45", 1, "José Reis Scherer", "(45)94508-7676", "13/10/2002", endereco2);
+        Endereco endereco5 = new Endereco("Rua Pindotiporã", 5, cidade1, 1555);
+        Proprietario proprietario1 = new Proprietario("585.837.370-45", 1, "Kaua Pessini Sito", "(44)99808-7706", "07/06/2003", endereco1);
+        Proprietario proprietario2 = new Proprietario("583.834.375-45", 1, "José Reis Scherer", "(44)94508-7676", "13/10/2002", endereco2);
         Animal gato = new Animal(1, "Bob", 10, 10.5, proprietario1, false, "");
-        Animal cachorro = new Animal(2, "Kauau", 8, 12.5, proprietario2, true, "17/10/2022");
+        Animal cachorro = new Animal(2, "Caramelo", 8, 12.5, proprietario2, true, "17/10/2022");
         Exame raiox = new Exame(1, "Radio X", 599.99);
         Exame castracao = new Exame(2, "Castração", 799.99);
         Medicamento paracetamal = new Medicamento(1, "paracetamal", 5, "06/06/2026", 69.0);
         Medicamento plasil = new Medicamento(2, "plasil", 10, "25/05/2024", 420.0);
-        Medico medico1 = new Medico("646442069", true, 1, "Dr Scherer", "(44)6987-8799", "12/04/1980", endereco3);
+        Medicamento cetoprofeno = new Medicamento(3, "Cetoprofeno", 20, "10/08/2027", 625.0);
+        Medico medico1 = new Medico("646442069", true, 1, "Dr Scherer", "(44)96987-8799", "12/04/1980", endereco3);
+        Medico medico2 = new Medico("254896513", false, 2, "Dr Reis", "(44)99645-0796", "15/09/1990", endereco4);
+        Medico medico3 = new Medico("865419785", true, 3, "Dr Sito", "(44)99465-0123", "10/05/1985", endereco5);
         Funcionario funcionario1 = new Funcionario("062.966.366-48", 1, "649216", "Mateus Martins Siqueira", "(44) 99808-7606", "13/03/1991", endereco4);
         Vacina vacina1 = new Vacina(1, "Raiva", 1, "25/05/2025", 795.99);
         Vacina vacina2 = new Vacina(1, "Tetano canino", 2, "12/06/2024", 595.95);
@@ -45,23 +47,13 @@ public class main {
         consulta.getListMedicamento().add(paracetamal);
         consulta.getListVacina().add(vacina1);
         consulta.calculaVlTotal();
-        
+
         BanhoTosa banhotosa = new BanhoTosa(1, cachorro, funcionario1, "18/09/2022");
         banhotosa.banho();
-        
+
         System.out.println(consulta.toString());
         System.out.println(banhotosa.toString());
-        
-     
-       //criar 2exames
-       //criar 2 medicamentso
-       //criar 2 vacinas
-       //criar medico
-       //criar funcionario
-       //criar consulta
-       //criar banho e 
-       
-        
+
     }
 
 }
